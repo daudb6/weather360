622ef743522137c5eb28ce1c0a40177e5d2c9af8
daudb6 <daudbasharat6@gmail.com>
2024-05-09 22:55:05 +0500

    complete this

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import { Routes,Route, RouterProvider } from 'react-router-dom';
-import './App.css';
-import { Navbar } from './Features/Navbar/Navbar';
-import { Report } from './Features/Report/Report';
-import { Cloud } from './Features/overcast/Cloud';
-import store from './store';
-import { Provider } from 'react-redux';
-import { Daud } from './test';
+import { Routes, Route, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./Features/Navbar/Navbar";
+import { Report } from "./Features/Report/Report";
+import { Cloud } from "./Features/overcast/Cloud";
+import store from "./store";
+import { Provider } from "react-redux";
 
 function App() {
-  
   return (
-    <Provider store = {store}>
-   <Navbar />
-   
-   <Routes>
+    <Provider store={store}>
+      <Navbar />
 
-   <Route path='/' element= {<Report/>}/>
-   <Route path='Cloud' element= {<Cloud />}/>
-   <Daud />
-   </Routes>
-   </Provider>
+      <Routes>
+        <Route path="/" element={<Report />} />
+        <Route path="Cloud" element={<Cloud />} />
+      </Routes>
+    </Provider>
   );
 }
 
